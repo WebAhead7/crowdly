1f050e34d40117db92d9f89fb6e6784aaf170532
Lujain <lujain.abdallatif@mail.huji.ac.il>
2020-11-18 11:47:29 +0200

    front end for registration

--- handlers/register.js
+++ handlers/register.js
@@ -1,8 +1,9 @@
 const db = require("../database/connection");
 
+// requiring path and fs to read register.html file and 
+
 function register(request, response) {
   response.writeHead(200, { "content-type": "text/html" });
-
   response.end("<h1>register</h1>");
 }
 
